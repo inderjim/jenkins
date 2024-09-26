51441d8699dc4f35cd2b824efbd594b7b6edaf5e
Indermohan <inderjim2006@gmail.com>
2024-09-26 22:00:34 +0200

    updated build script

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
-conan build .
+if [ ! -d "my-venv" ]; then
+apt install pipx
+python -m venv my-venv
+my-venv/bin/pip install conan
+fi
+
+rm -rf build
+./my-venv/bin/conan build .
 ./build/Release/common/demo
